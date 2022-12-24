c837008f17f5a47fecc026dc9748c18428a19b27
jrihanna <rihanna.zekri@gmail.com>
2022-12-25 03:29:35 +1100

    Organised imports

--- src/test/java/com/example/seek/service/CameraServiceTest.java
+++ src/test/java/com/example/seek/service/CameraServiceTest.java
@@ -11,10 +11,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.util.ReflectionTestUtils;
 
